959909625e6e93cf2d2ee3143b19309cb6e34fd6
nedal235 <38614159+nedal235@users.noreply.github.com>
2018-12-05 18:35:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const botconfig = require("./botconfig.json");
+const botconfig = require('botconfig.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const author = ["420227609163595777"];
